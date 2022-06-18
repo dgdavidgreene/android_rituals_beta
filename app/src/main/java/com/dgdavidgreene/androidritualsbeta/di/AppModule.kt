45659a38b1ca97f5aef7c5db57d768dd206cadb8
David Greene <dgdavidgreene@yahoo.com>
2022-06-17 19:43:48 -0700

    provideSqlDriver

--- app/src/main/java/com/dgdavidgreene/androidritualsbeta/di/AppModule.kt
+++ app/src/main/java/com/dgdavidgreene/androidritualsbeta/di/AppModule.kt
@@ -1,4 +1,36 @@
 package com.dgdavidgreene.androidritualsbeta.di
 
-class AppModule {
+import android.app.Application
+import com.dgdavidgreene.androidritualsbeta.AppDatabase
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.android.AndroidSqliteDriver
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSqlDriver(app: Application): SqlDriver {
+        return AndroidSqliteDriver(
+            schema = AppDatabase.Schema,
+            context = app,
+            name = "app.db"
+        )
+    }
+
+
+    /*@Provides
+    @Singleton
+    fun providePersonRepository(driver: SqlDriver): Repository {
+        return Repository(
+            AppDatabase.invoke(driver)
+        )
+    }*/
+
 }
